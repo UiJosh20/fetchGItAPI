358f2ecdcc8911d88795596cd8182f17a2f429c4
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-01-29 16:38:22 +0100

    fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/fetch/details/:id" element={<ContactDetails />}></Route>
           <Route path="/fetch" element={<Fetch />} />
         </Route>
